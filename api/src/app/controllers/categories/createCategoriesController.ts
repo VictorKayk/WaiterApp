c35679f5c084bc6298b35bb51d98b42433156dca
Victor Kayk <victorkayk77@gmail.com>
2022-12-30 17:13:33 -0300

    Add error handler

--- api/src/app/controllers/categories/createCategoriesController.ts
+++ api/src/app/controllers/categories/createCategoriesController.ts
@@ -2,16 +2,12 @@ import {Request, Response} from 'express';
 import {Category} from '../../models/CategoryModel';
 
 export async function createCategoriesController(req: Request, res: Response) {
-  try {
-    const { name, icon } = req.body;
+  const { name, icon } = req.body;
 
-    if (!name) return res.status(400).json({ error: 'Name is required '});
-    if (!icon) return res.status(400).json({ error: 'Icon is required '});
+  if (!name) return res.status(400).json({ error: 'Name is required '});
+  if (!icon) return res.status(400).json({ error: 'Icon is required '});
 
-    const category = await Category.create({ name, icon });
+  const category = await Category.create({ name, icon });
 
-    res.json(category);
-  } catch (e) {
-    console.log(e);
-  }
+  res.status(201).json(category);
 }
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import express from 'express';
 import {router} from './routes';
+import 'express-async-errors';
+import {errorHandler} from './app/middlewares/errorHandler';
 
 async function start() {
   try {
@@ -11,6 +13,7 @@ async function start() {
 
     app.use(express.json());
     app.use(router);
+    app.use(errorHandler);
 
     app.listen(5000, () => console.log('Running at http://localhost:5000'));
   } catch (e) {
